0b964e5d84775493a04a8bae859a917cb8d3151d
KyeongbinNoh <lylyby1010@gmail.com>
2023-04-17 00:00:57 -0400

    software project2

---
+++ backend/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        id:1,
+        name: "iPhone 12 Pro",
+        desc:"6.1-inch display",
+        price:999,
+        image:"https://res.cloudinary.com/dcoo3iqic/image/upload/v1680547115/GoodCart/iphone12pro_vqimjx.jpg"
+
+    },
+    {
+        id:2,
+        name: "iPhone 12",
+        desc:"5.4-inch display",
+        price:699,
+        image:"https://res.cloudinary.com/dcoo3iqic/image/upload/v1680547115/GoodCart/iphone12_ei6zp8.jpg"
+
+    },
+    {
+        id:3,
+        name: "Galaxy S",
+        desc:"6.5-inch display",
+        price:399,
+        image:"https://res.cloudinary.com/dcoo3iqic/image/upload/v1680547115/GoodCart/galaxyS_xfri0t.png"
+
+    },
+
+];
+
+module.exports = products;
---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import "react-toastify/dist/ReactToastify.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+
+import NavBar from './components/NavBar';
+import Cart from "./components/Cart";
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import CheckoutSuccess from './components/CheckoutSuccess';
+
+import Dashboard from "./components/admin/Dashboard";
+import Products from "./components/admin/Products";
+import Summary from "./components/admin/Summary";
+import CreateProduct from "./components/admin/CreateProduct";
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <ToastContainer/>
+        <NavBar/>
+        
+          <Routes>
+            <Route path="/" exact element={ <Home/> }/> 
+            <Route path="/cart" element={ <Cart/> }/> 
+            <Route path="/checkout-success" element={<CheckoutSuccess/>}/>
+            <Route path="/register" element={ <Register/> }/> 
+            <Route path="/login" element={ <Login/> }/>
+            <Route path="/admin" element={<Dashboard/>}>
+              <Route path="products" element={<Products/>}>
+                <Route path="create-product" element={<CreateProduct/>}/>
+              </Route>
+              <Route path="summary" element={<Summary/>}/>
+            </Route>
+            <Route path="/* " element={ <NotFound/> }/> 
+            
+          </Routes>
+           
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
